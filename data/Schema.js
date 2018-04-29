08bc5e4b7e72786f6de8593607b05385766072b8
Kristoffer Klevenberg <kristoffer@Testobars-Air.lan>
2018-04-29 13:27:45 +0200

    Grpahql session

--- data/Schema.js
+++ data/Schema.js
@@ -1,47 +1,116 @@
 var {
 	GraphQLSchema,
 	GraphQLObjectType,
+	GraphQLInputObjectType,
 	GraphQLString,
+	GraphQLNonNull,
 	GraphQLList,
+	GraphQLInt
 } = require('graphql');
+
 
 const User = new GraphQLObjectType({
 	name: 'User',
+	fields: () => ({
+		id: { type: GraphQLInt},
+		name: { type: GraphQLString},
+		username: { type: GraphQLString},
+		email: { type: GraphQLString},
+		phone: { type: GraphQLString},
+		website: { type: GraphQLString},
+		posts: {
+			type: new GraphQLList(Post),
+			resolve: async (source, args, {loaders}) => {
+				let posts = await loaders.getPostsLoader.load("all");
+				return posts.filter(post => post.userId == source.id)
+			}
+		},
+	})
+});
+const Post = new GraphQLObjectType({
+	name: 'Post',
 	fields:{
-		firstName: {
-			type: GraphQLString,
-			description: "This users first name",
-			resolve: () => "Ola"
+		id: { type: GraphQLInt},
+		userId: { type: GraphQLInt},
+		title: { type: GraphQLString},
+		body: { type: GraphQLString},
+		user: { 
+			type: User,
+			resolve: (source, args, {loaders}) => loaders.getUserLoader.load(source.userId)
 		},
-		lastName: {
-			type: GraphQLString,
-			description: "This users last name",
-			resolve: () => "Normann"
-		},
-		friends: {
-			type: GraphQLList(User),
-			description: "This users friends",
-			resolve: ()=> [1,2,3,4]
-		}
 	}
 });
 
 const RootQuery = new GraphQLObjectType({
 	name: 'Viewer',
 	fields:{
-		viewer: {
-			type: User,
-			resolve: () => ""
+		users: {
+			type: new GraphQLList(User),
+			args:{
+				id: { type: GraphQLInt},
+				username: { type: GraphQLString}
+			},
+			resolve: async (source, args, {loaders}) => {
+				let users = await loaders.getUsersLoader.load("all");
+				if (args.id)
+					return users.filter(user => user.id == args.id)
+				if (args.username)
+					return users.filter(user => user.username.indexOf(args.username) != -1)
+				return users;
+			}
 		},
-		servertime: {
-			type: GraphQLString,
-			resolve: () => new Date()
-		}
+		posts: {
+			type: GraphQLList(Post),
+			resolve: (source, args, {loaders}) => loaders.getPostsLoader.load("all")
+		},
 	}
 });
 
+const UserInputType = new GraphQLInputObjectType({
+	name: "UserInputType",
+	fields: () => ({
+		id: {type: new GraphQLNonNull(GraphQLInt)},
+		name: {type: GraphQLString},
+	})
+});
+
+const RootMutations = new GraphQLObjectType({
+	name: "RootMutations",
+	description: "Make changes to a user",
+	fields: () => ({
+		createUser: {
+			type: User,
+			name: "Create new user",
+			description: "another one..",
+			args: {
+				user: {type: UserInputType}
+			},
+			resolve: (source, args, {loaders}) => {
+				//resolve function and return new object
+				return loaders.getUserLoader.load(args.id)
+			}
+		},
+		updateUser:{
+			type: User,
+			name: "Update users name",
+			description: "more details",
+			args: {
+				id: {type: new GraphQLNonNull(GraphQLInt)},
+				name: {type: GraphQLString},
+			},
+			resolve: (source, args, {loaders}) => {
+				//resolve function and return new object
+				return loaders.getUserLoader.load(args.id)
+			}
+		},
+	})
+
+})
+
 const Schema = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation: RootMutations
 });
 
 module.exports = Schema;
+
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const Schema = require('./data/Schema.js');
+const Dataloader = require('dataloader');
 
 const app = express();
+const api = require("./lib/restApi");
 
-app.use('/graphql', graphqlHTTP({
-  schema: Schema,
-  graphiql: true,
+app.use('/graphql', graphqlHTTP(req => {
+
+	const loaders = {
+		getUsersLoader: new Dataloader(keys => Promise.all(keys.map(api.getUsers))),
+		getUserLoader:  new Dataloader(keys => Promise.all(keys.map(key => api.getUser(key)))),
+		getPostsLoader:  new Dataloader(keys => Promise.all(keys.map(api.getPosts))),
+		getPostLoader:  new Dataloader(keys => Promise.all(keys.map(key => api.getPost(key)))),
+	}
+
+	return{
+		schema: Schema,
+		graphiql: true,
+		context:{
+			loaders
+		}
+	}
+
 }));
 
 app.listen(4000);
